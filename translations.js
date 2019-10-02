927011e949d11a3fd99ae76dff4067dd28af98bc
Léo DESIGAUX <leodesigaux@gmail.com>
2019-10-02 11:55:10 +0200

    Add internationalization
    Adds i18n + english basic translations
    Only basic ui labels are translated
    Adds a lang selector in top right

---
+++ translations.js
@@ -0,0 +1,22 @@
+const translations = {
+  en: {
+    global: {
+      title: 'Libre CGM configurations',
+      description: 'Would you use MiaoMiao or BluCon to get glycemic data live on your device or remote device without knowing what you need? Choose from the options below which scenario is yours and see what configurations you can put together.',
+      configuration: 'Configuration',
+      collection_application: 'Data Collection Application',
+      transfer_application: 'Data is transmitted via',
+      receiver_application: 'Remote Reading Application',
+    }
+  },
+  bs: {
+    global: {
+      title: 'Libre CGM konfiguracije',
+      description: 'Koristili biste MiaoMiao ili BluCon da dobivate podatke o glikemiji uživo na svom ili udaljenom uređaju, a ne znate što trebate? Među opcijama niže odaberite koji scenarij je vaš i pogledajte koje konfiguracije možete sastaviti.',
+      configuration: 'Konfiguracija',
+      collection_application: 'Aplikacija za prikupljanje podataka',
+      transfer_application: 'Podaci se prenose preko',
+      receiver_application: 'Aplikacija za udaljeno čitanje podataka',
+    }
+  }
+}
